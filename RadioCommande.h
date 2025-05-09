df753f0934009897ceef81169efcedaa6abc98ed
avillatte <145434200+avillatte@users.noreply.github.com>
2025-05-09 15:28:21 +0200

    Update RadioCommande.h

--- RadioCommande.h
+++ RadioCommande.h
@@ -1,36 +1,20 @@
-#ifndef JOYSTICK_H
-#define JOYSTICK_H
-
 #include <QObject>
-#include <QSerialPort>
+#include <QGamepad>
 #include <QTimer>
 #include <QNetworkAccessManager>
 
 class Joystick : public QObject
 {
     Q_OBJECT
-
 public:
-    explicit Joystick(QObject *parent = nullptr);
+    Joystick(QObject *parent = nullptr);
     ~Joystick();
 
-    void setRestartClicked(bool clicked);
-    void setRthDistance(float distance);
-
 private slots:
-    void lireDonneesSerie();
     void envoyerRequete();
 
 private:
-    QSerialPort *serial;
+    QGamepad *gamepad;
     QTimer *sendTimer;
     QNetworkAccessManager *networkManager;
-
-    int gazValue = 0;
-    int lacetValue = 0;
-    int tangageValue = 0;
-    int roulisValue = 0;
-    QString buffer;
 };
-
-#endif // JOYSTICK_H
